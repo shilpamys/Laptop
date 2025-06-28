f88560cede72f44bb08a0870f4f36735cde0da31
shilpad <shilpamys@gmail.com>
2025-06-28 22:26:22 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+                    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+           Laptop l1=new Laptop("Mac Book Air", 16, "Intel M1");
+           Laptop l2=new Laptop("MacBook Pro", 32, "Apple M2");
+           l1.displayInfo();
+           l2.displayInfo();
+
+
+
+    }
+}
